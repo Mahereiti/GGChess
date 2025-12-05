543df23727bf9ca9a60b0456906d39fe9a0eaa00
Samadi <chaymae.samadi16@gmail.com>
2025-12-05 10:18:51 +0100

    a

---
+++ GGChess/src/Database.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class Database {
+
+    private static final String URL = "jdbc:sqlite:database/GGchess.db"; // chemin relatif
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
