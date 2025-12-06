1e31b68c7ad79a6632c3919a9f23eab2fb6183ce
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2025-12-06 17:23:10 +0100

    reset game + update ui

--- GGChess/src/KillPiecesPanel.java
+++ GGChess/src/KillPiecesPanel.java
@@ -1,7 +1,7 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
@@ -22,16 +22,21 @@ public class KillPiecesPanel extends JPanel {
 		whiteKillPanel = new JPanel(null);
 		blackKillPanel = new JPanel(null);
 		
-		whiteKillPanel.setBackground(Color.gray);
-		blackKillPanel.setBackground(Color.black);		
+		whiteKillPanel.setOpaque(false);
+		blackKillPanel.setOpaque(false);
 		
-		// Layout vertical
+		whiteKillPanel.setPreferredSize(new Dimension(this.getWidth(), 200));
+		blackKillPanel.setPreferredSize(new Dimension(this.getWidth(), 200));
+		
+		// Layout vertical with flexible spaces
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.add(Box.createGlue());
 		this.add(whiteKillPanel);
 		this.add(blackKillPanel);
-		
+		this.add(Box.createGlue());
 		
 		this.setPreferredSize(new Dimension((g.d.width-(g.d.height-200))/2, g.d.height-200));
+		this.setOpaque(false);
 	}
 	
 	public void killPiece(Piece p) {
@@ -49,9 +54,9 @@ public class KillPiecesPanel extends JPanel {
 		Clickable kill = new Clickable();
 		kill.setIcon(p.getIcon());
 		
-		int xStart = (this.getWidth()-310)/2;			// start point : (width - (7pieces*30+100))/2
+		int xStart = (this.getWidth()-310)/2;			// start x : (width - (7pieces*30+100))/2
 		int xPiece = ((whiteKilled.size()-1)*30)%240;	// each piece +30px, reset every 8 pieces
-		int yPiece = ((whiteKilled.size()-1)/8)*100;	// new row every 8 pieces
+		int yPiece = ((whiteKilled.size()-1)/8)*80;	// new row every 8 pieces
 		
 		kill.setBounds(xStart+xPiece, yPiece, 100, 100);
 		whiteKillPanel.add(kill);
@@ -62,12 +67,22 @@ public class KillPiecesPanel extends JPanel {
 		// new clickable image for the newly black killed piece
 		Clickable kill = new Clickable();
 		kill.setIcon(p.getIcon());
-		int xStart = (this.getWidth()-310)/2;			// start point : (width - (7pieces*30+100))/2
-		int xPiece = ((whiteKilled.size()-1)*30)%240;	// each piece +30px, reset every 8 pieces
-		int yPiece = ((whiteKilled.size()-1)/8)*100;	// new row every 8 pieces
+		int xStart = (this.getWidth()-310)/2;			// start x : (width - (7pieces*30+100))/2
+		int xPiece = ((blackKilled.size()-1)*30)%240;	// each piece +30px, reset every 8 pieces
 		
-		kill.setBounds(xStart+xPiece, yPiece, 100, 100);
+		int yStart = blackKillPanel.getHeight()-100;	// start y
+		int yPiece = ((blackKilled.size()-1)/8)*80;	// new row every 8 pieces
+		
+		kill.setBounds(xStart+xPiece, yStart-yPiece, 100, 100);
 		blackKillPanel.add(kill);
 		repaint();		// force panel to redraw
+	}
+
+	public void reset() {
+		whiteKillPanel.removeAll();
+		blackKillPanel.removeAll();
+		whiteKilled.clear();
+		blackKilled.clear();
+		this.repaint();
 	}
 }
