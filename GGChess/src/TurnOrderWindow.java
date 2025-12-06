e92dccd4806e37406b2af4f179aa3becb07f5a30
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2025-12-06 17:20:01 +0100

    create turn order window

---
+++ GGChess/src/TurnOrderWindow.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class TurnOrderWindow extends BaseFrame {
+	private String firstPlayer;
+
+	public TurnOrderWindow(String firstPlayer) {
+		super("Tour", 600, 200);
+		this.firstPlayer = firstPlayer;
+	}
+
+	@Override
+	public void setContent() {
+		JLabel text = new JLabel("A "+firstPlayer+" de jouer !");
+		text.setForeground(Color.WHITE);
+		text.setFont(new Font("Sans-serif", Font.BOLD, 40));
+		text.setAlignmentX(CENTER_ALIGNMENT);
+		
+		contentPanel.add(text);
+	}
+}
