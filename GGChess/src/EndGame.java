3fdf14b0ba7cc86b7ea71a1d27619ab21b0b1d47
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2025-12-06 17:21:38 +0100

    create end game window

---
+++ GGChess/src/EndGame.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class EndGame extends BaseFrame {
+	private Game g;
+	private Player actualPlayer, winner;
+
+	public EndGame(Game g, Player actualPlayer, Player winner) {
+		super("Fin de partie", 600, 500);
+		this.g = g;
+		this.actualPlayer = actualPlayer;
+		this.winner = winner;
+	}
+
+	@Override
+	public void setContent() {
+		JLabel text = new JLabel();
+		if (winner==null) text.setText("Match null !");
+		else if (actualPlayer==winner) {
+			text.setText("Vous avez gagné !");
+		} else text.setText("Vous avez perdu...");
+		
+		text.setForeground(Color.white);
+		text.setFont(new Font("Sans-serif", Font.BOLD, 40));
+		text.setAlignmentX(CENTER_ALIGNMENT);
+		
+		Clickable menuBtn = new Clickable("/galaxy/menu.png", 0, 0);
+		Clickable restartBtn = new Clickable("/galaxy/menu.png", 0, 0);
+		
+		menuBtn.addActionListener(e-> {
+			g.windowPrincipal.showMenu();
+			this.dispose();		// close window
+		});
+		
+		menuBtn.addActionListener(e-> {
+			g.windowPrincipal.showMenu();
+			this.dispose();		// close window
+		});
+		
+		restartBtn.addActionListener(e-> {
+			g.resetGame();
+			this.dispose();
+		});
+		
+	}
+
+}
