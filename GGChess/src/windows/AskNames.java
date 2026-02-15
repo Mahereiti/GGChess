1a3d2610943ec63ea001730ce711161bba4cb9ed
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2026-02-15 16:58:11 +0100

    affichage asknames + saisie direct/clic entrée

--- GGChess/src/windows/AskNames.java
+++ GGChess/src/windows/AskNames.java
@@ -2,6 +2,8 @@ package windows;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.Dimension;
 import javax.swing.*;
 
@@ -14,15 +16,34 @@ public class AskNames extends BaseFrame {
     private JLabel textLabel;
     private JButton validateBtn;
 
-    private String player1Name;
     private int step = 1; // 1 for Player 1 and 2 for Player 2
     private GGChess ggchess;
     
     public AskNames(GGChess ggchess) {
         super("Players Names", 600, 350);
-        closeBtn.setVisible(false);
+        // replace closeBtn by emptyIcon (title still centered)
+        Icon original = closeBtn.getIcon();
+        if (original != null) {
+            Icon emptyIcon = new Icon() {
+                @Override
+                public int getIconWidth() {
+                    return original.getIconWidth();
+                }
+
+                @Override
+                public int getIconHeight() {
+                    return original.getIconHeight();
+                }
+
+                @Override
+                public void paintIcon(java.awt.Component c, java.awt.Graphics g, int x, int y) {
+                }
+            };
+            closeBtn.setIcon(emptyIcon); // même taille, mais invisible
+            closeBtn.setEnabled(false);
+        }
         this.ggchess = ggchess;
-        setContent();
+        this.setContent();
     }
 
     @Override
@@ -30,7 +51,7 @@ public class AskNames extends BaseFrame {
 
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 
-        textLabel = new JLabel("Veuillez entrer le nom du joueur 1");
+        textLabel = new JLabel("Joueur Noir");
         textLabel.setForeground(Color.WHITE);
         textLabel.setFont(new Font("SansSerif", Font.BOLD, 28));
         textLabel.setAlignmentX(CENTER_ALIGNMENT);
@@ -39,6 +60,14 @@ public class AskNames extends BaseFrame {
         nameField.setMaximumSize(nameField.getPreferredSize());
         nameField.setFont(new Font("SansSerif", Font.BOLD, 24));
         nameField.setAlignmentX(CENTER_ALIGNMENT);
+        nameField.setFocusable(true);
+        nameField.requestFocusInWindow();	//autofocus
+        nameField.addKeyListener(new KeyAdapter() {	// touch enter
+        	@Override
+        	public void keyPressed(KeyEvent e) {
+        		if (e.getKeyCode() == KeyEvent.VK_ENTER) onValidate();
+        	}
+        });
         
         Dimension fieldSize = new Dimension(400, 50); // largeur 400px, hauteur 50px
         nameField.setPreferredSize(fieldSize);
@@ -46,7 +75,6 @@ public class AskNames extends BaseFrame {
 
         validateBtn = new Clickable("/galaxy/validate.png", 0, 0);
         validateBtn.setAlignmentX(CENTER_ALIGNMENT);
-
         validateBtn.addActionListener(e -> onValidate());
 
         contentPanel.add(textLabel);
@@ -54,7 +82,7 @@ public class AskNames extends BaseFrame {
         contentPanel.add(validateBtn);
     }
 
-    private void onValidate() {
+    public void onValidate() {
         String name = nameField.getText().trim();
 
         if (name.isEmpty()) {
@@ -63,23 +91,19 @@ public class AskNames extends BaseFrame {
         }
 
         if (step == 1) {
-            //for the player 1
-            player1Name = name;
             //for the player 2
             step = 2;
-            textLabel.setText("Veuillez entrer le nom du joueur 2");
+            textLabel.setText("Joueur Blanc");
             nameField.setText("");
             ggchess.getGamePanel().playersPanel.getCurrentPlayer().setName(name);
             ggchess.getGamePanel().playersPanel.updatePlayerName(true, name);
 
         } else {
-            String player2Name = name;
             ggchess.getGamePanel().playersPanel.getCurrentPlayer().setName(name);
             ggchess.getGamePanel().playersPanel.updatePlayerName(false, name);
             //We close the window
             dispose();
             new TurnOrderWindow(ggchess.getGamePanel().playersPanel.getCurrentPlayerName());
         }
-        
     }
 }
